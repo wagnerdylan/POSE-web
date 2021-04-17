6cc5c390b79853557da267a0ea22db273919f81b
Dylan Wagner <vstrideultimate@gmail.com>
2021-04-16 23:37:45 -0400

    feat: added server websocket support

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
+use actix::prelude::*;
 use actix_files::Files;
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+
+struct SimWs;
+
+impl Actor for SimWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for SimWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        todo!()
+    }
+}
+
+async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    ws::start(SimWs {}, &req, stream)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(Files::new("/", "./web/dist/").index_file("index.html")))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/ws/").route(web::get().to(ws_index)))
+            .service(Files::new("/", "./web/dist/").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
